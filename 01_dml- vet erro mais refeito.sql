2e4b0b98aa1a480b97c91608fa9c3be5fd965cfd
FIC <FIC@info.local>
2020-08-09 15:49:28 -0300

    erro de prototipo logico, refazer ligações de fk e pk para seguir correto... gambiarras de código não deu mais certo aqui

--- 01_dml- vet erro mais refeito.sql
+++ 01_dml- vet erro mais refeito.sql
@@ -1,13 +1,2 @@
---inserindo dados
-INSERT INTO clinica (NomeClinica, enderecoClinica) VALUES ('teste vet', 'Av logo ali');
-INSERT INTO tipopet (TipoAnimal) VALUES ('gato');
-INSERT INTO dono (Nome) VALUES ('testenome');
-INSERT INTO raca (DescricaoRaca, IdTipoPet) VALUES ('teste', 2);
-INSERT INTO pet (Nome, DataNascimento, IdDono, IdRaca) VALUES ('Testenomepet', '2020-08-21', 1, 2);
-INSERT INTO atendimento (IdVeterinario, DataAtendimento, IdPet) VALUES ( 1, '2020-06-22', 'OK', 1);
-INSERT INTO veterinario (IdVeterinario,idclinica, Nome) VALUES (1,1, 'Dr.teste');
---alterando dados
-UPDATE veterinario SET
-CRVM = 43551
-WHERE IdVeterinario = 1; 
+
 
